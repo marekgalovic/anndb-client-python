abd8960bfc3e62c047499f0041a77c2b4835271c
Marek Galovic <galovic.galovic@gmail.com>
2020-06-20 21:28:37 +0200

    Fix dependency

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 
+VERSION = '0.0.2'
+
 setup(name='anndb',
-    version='0.0.1',
+    version=VERSION,
     description='AnnDB Client',
     author='Marek Galovic',
     author_email='galovic.galovic@gmail.com',
     license='MIT',
     url='https://github.com/marekgalovic/anndb-client-python',
-    download_url='https://github.com/marekgalovic/anndb-client-python/archive/0.0.1.tar.gz',
+    download_url='https://github.com/marekgalovic/anndb-client-python/archive/%s.tar.gz' % VERSION,
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
-        'grpc'
+        'grpcio'
     ],
 )
